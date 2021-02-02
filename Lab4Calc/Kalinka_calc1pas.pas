c88a4f779631d6807ecb1acc511d3801eaf66816
Nakama3942 <73797846+Nakama3942@users.noreply.github.com>
2021-02-03 00:50:55 +0200

    Add files via upload

---
+++ Lab4Calc/Kalinka_calc1pas.pas
@@ -0,0 +1,14 @@
+﻿program Kalinka_calc1pas;
+uses crt;
+const c=0.2; d=0.1;
+var x,y,z:real;
+begin
+  x:=(exp(c))-d; y:=d*(exp(3*ln(cos(x)))); z:=(exp((c+1)*ln(y))+x)/tan(y);
+  writeln('Начальное значения C: ', c, ' и D: ', d, ' .');
+  writeln('Исходя из этих значений я получаю X, Y и Z, а именно:');
+  writeln('X=', x);
+  writeln('Y=', y);
+  writeln('Z=', z);
+  writeln('Программа выполнена!');
+  readkey;
+end.
---
+++ Lab4Calc/Kalinka_quest26.pas
@@ -0,0 +1,11 @@
+﻿program Kalinka_quest26_Lab4;
+uses crt;
+const f=-231.09273286123;
+var x:real;
+begin
+  x:=5.34;
+  write('Получено следующие результаты: ');
+  writeln('f=', f:6:3, ' для x=', x);
+  writeln('   Конец   ');
+  readkey;
+end.
